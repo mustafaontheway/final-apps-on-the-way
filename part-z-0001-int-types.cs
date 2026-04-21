aaf4a9070403bdd01f0b34241a49b599540fad5c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-21 19:49:47 +0300

    Create part-z-0001-int-types.cs

---
+++ part-z-0001-int-types.cs
@@ -0,0 +1,22 @@
+byte age = 20;
+
+short year = 2026;
+
+int diff = -550_000;
+
+long totalSales = 4_353_921_200;
+
+ushort birthYear = 1900; // System.UInt16
+
+uint monthlySales = 420_000; // System.UInt32
+
+ulong population = 1_400_000_000; // System.UInt64
+
+Console.WriteLine(age);
+Console.WriteLine(year);
+Console.WriteLine(diff);
+Console.WriteLine(totalSales);
+Console.WriteLine(monthlySales);
+Console.WriteLine(population);
+
+// dotnet run
