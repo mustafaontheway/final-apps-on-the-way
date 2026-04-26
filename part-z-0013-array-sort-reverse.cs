0c0c4b3c1f9334d90212c1c4c9ba5dd30cdf7831
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-26 19:43:06 +0300

    Create part-z-0013-array-sort-reverse.cs

---
+++ part-z-0013-array-sort-reverse.cs
@@ -0,0 +1,14 @@
+string[] names = {"Ayhan", "Aykan", "Ayman", "Aykul", "Ayben", "Aygül", "Aysel", "Aysu"};
+
+Array.Sort(names);
+
+System.Console.WriteLine(names[0]);
+
+Array.Reverse(names);
+
+System.Console.WriteLine(names[0]);
+
+// Ayben
+// Aysu
+
+// dotnet run
