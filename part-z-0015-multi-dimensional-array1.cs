753dc284fed12995984c8338738bba1252a32a10
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-26 21:26:26 +0300

    Create part-z-0015-multi-dimensional-array1.cs

---
+++ part-z-0015-multi-dimensional-array1.cs
@@ -0,0 +1,19 @@
+uint[,] quarterlySales = new uint[3, 4];
+
+quarterlySales[0, 0] = 550_000;
+quarterlySales[0, 1] = 650_000;
+quarterlySales[0, 2] = 750_000;
+quarterlySales[0, 3] = 850_000;
+
+quarterlySales[1, 0] = 999_000;
+quarterlySales[1, 1] = 899_000;
+quarterlySales[1, 2] = 799_000;
+quarterlySales[1, 3] = 699_000;
+
+quarterlySales[2, 0] = 666_666;
+quarterlySales[2, 1] = 566_666;
+quarterlySales[2, 2] = 466_666;
+quarterlySales[2, 3] = 366_666;
+
+
+// dotnet run
