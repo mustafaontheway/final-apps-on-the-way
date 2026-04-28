9add87abae2b708a5ca7178eee5a0501ff099bc3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-28 19:40:04 +0300

    Create part-z-0026-list-spread1.cs

---
+++ part-z-0026-list-spread1.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+List<byte> ages1 = new List<byte> {77, 47, 100};
+
+List<byte> ages2 = new List<byte> {99, 89, 79};
+
+List<byte> allAges = [..ages1, ..ages2];
+
+foreach (var age in allAges)
+{
+    System.Console.WriteLine($"Age: {age}");
+}
+
+// Age: 77
+// Age: 47
+// Age: 100
+// Age: 99
+// Age: 89
+// Age: 79
+
+// dotnet run
