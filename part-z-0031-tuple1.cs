a6fb266124c8067c4886359331ea2d70f09fdd46
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-28 20:27:59 +0300

    Create part-z-0031-tuple1.cs

---
+++ part-z-0031-tuple1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+
+var person1 = (Name: "Hakan Bilir", Age: 19);
+
+System.Console.WriteLine(person1.Name);
+
+System.Console.WriteLine(person1.Age);
+
+// Hakan Bilir
+// 19
+
+
+// dotnet run
