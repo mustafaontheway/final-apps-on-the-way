29b4408c5c1727d7e926d08306ee57e478296550
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-28 19:33:01 +0300

    Create part-z-0024-list2.cs

---
+++ part-z-0024-list2.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+List<byte> ages = new List<byte> {77, 47, 100};
+
+ages.Add(17);
+ages.Add(44);
+ages.Add(27);
+ages.Add(7);
+
+System.Console.WriteLine(ages.Count); // 7
